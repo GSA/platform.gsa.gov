453b3e9a4dd218d9efb4afd509cf67a31401b059
ctacrob <rmorris@ctacorp.com>
2018-05-03 22:55:56 -0400

    CMPCR-558: Plugin upgrades: Google Analytics 7.0.4 to 7.0.5. Jetpack 5.9 to 6.1. Members 2.0.2 to 2.1.0. Profile Builder 2.7.9 to 2.8.1. Share Buttons (AddThis) 6.1.2 to 6.1.5. WP Security Audit Log 3.1.4 to 3.1.7.

--- app/platform.gsa.gov/wp-content/plugins/wp-security-audit-log/classes/Sensors/Content.php
+++ app/platform.gsa.gov/wp-content/plugins/wp-security-audit-log/classes/Sensors/Content.php
@@ -226,6 +226,7 @@ class WSAL_Sensors_Content extends WSAL_AbstractSensor {
 					'TagLink' => $term_link,
 					'old_desc' => $old_desc,
 					'new_desc' => $new_desc,
+					'ReportText' => $old_desc . '|' . $new_desc,
 				)
 			);
 		}
@@ -912,6 +913,7 @@ class WSAL_Sensors_Content extends WSAL_AbstractSensor {
 					'OldUrl' => $old_link,
 					'NewUrl' => $new_link,
 					$editor_link['name'] => $editor_link['value'],
+					'ReportText' => '"' . $old_link . '"|"' . $new_link . '"',
 				)
 			);
 			return 1;
@@ -1053,6 +1055,18 @@ class WSAL_Sensors_Content extends WSAL_AbstractSensor {
 					$event = 2002;
 				}
 				if ( $event ) {
+					if ( 2002 === $event ) {
+						// Get Yoast alerts.
+						$yoast_alerts = $this->plugin->alerts->get_alerts_by_sub_category( 'Yoast SEO' );
+
+						// Check all alerts.
+						foreach ( $yoast_alerts as $alert_code => $alert ) {
+							if ( $this->plugin->alerts->WillOrHasTriggered( $alert_code ) ) {
+								return 0; // Return if any Yoast alert has or will trigger.
+							}
+						}
+					}
+
 					$editor_link = $this->GetEditorLink( $oldpost );
 					$this->plugin->alerts->Trigger(
 						$event, array(
